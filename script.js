ec9ea747ed7b8350e0a77055c034961439253d56
MikeYoulend <mikeyoulend@gmail.com>
2024-01-20 16:09:16 +0100

    Stopwatch

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let [seconds, minutes, hours] = [0, 0, 0];
 let displayTime = document.getElementById("displayTime");
+let timer = null;
 
 function stopwatch() {
 	seconds++;
@@ -11,8 +12,17 @@ function stopwatch() {
 			hours++;
 		}
 	}
+
+	let h = hours < 10 ? "0" + hours : hours;
+	let m = minutes < 10 ? "0" + minutes : minutes;
+	let s = seconds < 10 ? "0" + seconds : seconds;
+
+	displayTime.innerHTML = hours + ":" + minutes + ":" + seconds;
 }
 
 function watchStart() {
-	setInterval(stopwatch, 1000);
+	if (timer != null) {
+		clearInterval(timer);
+	}
+	timer = setInterval(stopwatch, 1000);
 }
